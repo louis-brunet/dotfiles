6dc212bda4069c7dd0f0235859fb476bf89daae4
Louis Brunet <louis.brunet1@grenoble-inp.org>
2023-08-15 14:51:14 +0200

    feat: add some git aliases; bin/e to open editor

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,2 +1,8 @@
-alias gs="git status"
+# Remove `+` and `-` from start of diff lines; just rely upon color.
+alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+
+alias gc='git commit'
+alias gco='git checkout'
+
+alias gs="git status -sb"
 
