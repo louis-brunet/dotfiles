7269d801528baa831eaae9013ddb21ac2c737eb8
Louis Brunet <louis.brunet1@grenoble-inp.org>
2023-08-26 11:53:58 +0200

    feat(node): install node LTS and npm via nvm
    Don't install node and npm if `which node` and `which npm` don't error.

---
+++ node/install.sh
@@ -0,0 +1,24 @@
+# install node and npm with nvm
+set -e 
+
+if [ ! $(which nvm) ]; then
+    XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config} PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash'
+
+    # load nvm for current terminal session
+    # this is also done by the oh-my-zsh plugin "nvm"
+    export NVM_DIR="$(printf %s "${XDG_CONFIG_HOME:-$HOME/.config}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" # This loads nvm
+    
+    nvm --version
+    echo "✅ installed nvm (run exec zsh)"
+fi
+
+if [ ! $(which node) ]; then
+    nvm install --lts
+    nvm use --lts
+
+    echo "node $(node --version)"
+    echo "npm $(npm --version)"
+    echo "✅ installed node LTS and npm"
+fi
+
