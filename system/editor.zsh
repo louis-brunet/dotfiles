7a6096a548d1fdedc91107b633eb3928be2804e4
Louis Brunet <louis.brunet1@grenoble-inp.org>
2023-09-13 15:21:52 +0200

    fix(nvim): comment unused `$SSH_CONNECTION` check

--- system/editor.zsh
+++ system/editor.zsh
@@ -1,9 +1,11 @@
 # Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='nvim'
-else
-  export EDITOR='nvim'
-fi
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='nvim'
+# else
+#   export EDITOR='nvim'
+# fi
+
+export EDITOR='nvim'
 
 # use nvim as the default git editor
 export GIT_EDITOR=$EDITOR
