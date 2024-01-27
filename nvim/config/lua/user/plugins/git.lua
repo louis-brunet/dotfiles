7183fd49baf12e7401327c7229b16557c64a6ce5
Louis Brunet <louis.brunet1@grenoble-inp.org>
2024-01-27 11:08:34 +0100

    nvim: make git keymaps more cohesive

--- nvim/config/lua/user/plugins/git.lua
+++ nvim/config/lua/user/plugins/git.lua
@@ -3,14 +3,14 @@ local M = {
     -- Git related plugins
     {
         'tpope/vim-fugitive',
-        event='VeryLazy',
+        event = 'VeryLazy',
     },
     -- 'tpope/vim-rhubarb',
 
     {
         -- Adds git related signs to the gutter, as well as utilities for managing changes
         'lewis6991/gitsigns.nvim',
-        event='VeryLazy',
+        event = 'VeryLazy',
         opts = {
             -- See `:help gitsigns.txt`
             signs = {
@@ -23,16 +23,21 @@ local M = {
             on_attach = function(bufnr)
                 vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
                     { buffer = bufnr, desc = '[G]it: [P]revious Hunk' })
+                vim.keymap.set('n', '[g', require('gitsigns').prev_hunk,
+                    { buffer = bufnr, desc = '[G]it: Previous Hunk' })
+
                 vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
                     { buffer = bufnr, desc = '[G]it: [N]ext Hunk' })
+                vim.keymap.set('n', ']g', require('gitsigns').next_hunk,
+                    { buffer = bufnr, desc = '[G]it: Next Hunk' })
+
                 vim.keymap.set('n', '<leader>gr', require('gitsigns').reset_hunk,
                     { buffer = bufnr, desc = '[G]it: [R]eset Hunk' })
-                vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk_inline,
-                    { buffer = bufnr, desc = 'Git: [P]review [H]unk' })
+                vim.keymap.set('n', '<leader>gh', require('gitsigns').preview_hunk_inline,
+                    { buffer = bufnr, desc = '[G]it: Preview [H]unk' })
             end,
         },
     },
 }
 
 return M
-
