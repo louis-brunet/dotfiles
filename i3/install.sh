fc5a08e8aac662d2a7ff4f9445d52179609174db
Louis Brunet <louis.brunet1@grenoble-inp.org>
2024-01-28 16:09:00 +0100

    i3: uncomment config

--- i3/install.sh
+++ i3/install.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
-# set -e 
-#
-# sudo apt-get install -y i3
-# i3 --version
-# echo "✅ installed i3 window manager, log out and back in if needed"
-#
-# # Install playerctl for media keybinds (pause, play, stop)
-# if ! which playerctl; then
-#     playerctl_download_path=$HOME/Downloads/playerctl.deb
-#     curl -o "$playerctl_download_path" -L https://github.com/altdesktop/playerctl/releases/download/v2.4.1/playerctl-2.4.1_amd64.deb
-#     sudo dpkg -i "$playerctl_download_path"
-#
-#     playerctl --version
-#     echo "✅ installed playerctl for media keys in i3"
-# else
-#     playerctl --version
-#     echo "✅ playerctl already installed, skipping"
-# fi 
-#
-# # Install i3blocks for the i3 status bar
-# if ! which i3blocks; then
-#     sudo apt-get install -y i3blocks
-#     i3blocks -V
-#
-#     echo "✅ installed i3blocks for media keys in i3"
-# else
-#     i3blocks -V
-#
-#     echo "✅ i3blocks already installed, skipping"
-# fi 
+
+set -e 
+sudo apt-get install -y i3
+i3 --version
+echo "✅ installed i3 window manager, log out and back in if needed"
+
+# Install playerctl for media keybinds (pause, play, stop)
+if ! which playerctl; then
+    playerctl_download_path=$HOME/Downloads/playerctl.deb
+    curl -o "$playerctl_download_path" -L https://github.com/altdesktop/playerctl/releases/download/v2.4.1/playerctl-2.4.1_amd64.deb
+    sudo dpkg -i "$playerctl_download_path"
+
+    playerctl --version
+    echo "✅ installed playerctl for media keys in i3"
+else
+    playerctl --version
+    echo "✅ playerctl already installed, skipping"
+fi 
+
+# Install i3blocks for the i3 status bar
+if ! which i3blocks; then
+    sudo apt-get install -y i3blocks
+    i3blocks -V
+
+    echo "✅ installed i3blocks for media keys in i3"
+else
+    i3blocks -V
+
+    echo "✅ i3blocks already installed, skipping"
+fi 
 
