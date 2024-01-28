c6cae6524b3a87a729dc30842c29cb0c35fbf843
Louis Brunet <louis.brunet1@grenoble-inp.org>
2024-01-28 13:56:05 +0100

    nvim: don't download nvim appimage again if there is one present (ie no auto update)

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,13 +3,15 @@
 set -e
 # echo "⚠️  TODO: uncomment nvim install script (commented bc docker ubuntu doesn't support snap)"
 
-# download latest stable appimage, save it as $HOME/bin/nvim with execution permissions
 # $HOME/bin should be in PATH
-echo "🌐 pulling latest stable nvim appimage"
 nvim_path="$HOME/bin/nvim" 
-mkdir -p "$(dirname "$nvim_path")"
-curl -o "$nvim_path" -L https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x "$nvim_path"
+if [[ ! -x "$nvim_path" ]]; then
+    # download latest stable appimage, save it as $HOME/bin/nvim with execution permissions
+    echo "🌐 pulling latest stable nvim appimage"
+    mkdir -p "$(dirname "$nvim_path")"
+    curl -o "$nvim_path" -L https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+    chmod u+x "$nvim_path"
+fi
 
 # sudo snap install nvim --classic
 nvim --version
