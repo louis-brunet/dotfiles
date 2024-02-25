ac0f23dad4aadf002c8cd8ba8c325ca8922f02b0
Louis Brunet <123louisb@gmail.com>
2024-02-25 10:27:30 +0100

    tmux: include LOCAL_TMUX_SESSIONIZER_DIRS if it is defined (in .localrc)

--- tmux/env.zsh
+++ tmux/env.zsh
@@ -1,2 +1,8 @@
 export TMUX_SESSIONIZER_DIRS="$PROJECTS"
 
+# possibly defined in ~/.localrc
+if [[ -n "$LOCAL_TMUX_SESSIONIZER_DIRS" ]]; then
+    export TMUX_SESSIONIZER_DIRS="$LOCAL_TMUX_SESSIONIZER_DIRS:$TMUX_SESSIONIZER_DIRS"
+fi
+
+# vi:ft=sh:
