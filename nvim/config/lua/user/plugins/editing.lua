afca92009038516e9ad91346147f381c30d02e90
Louis Brunet <123louisb@gmail.com>
2024-03-02 10:31:32 +0100

    nvim: add keymap <leader>st = TodoTelescope, declare which-key groups

--- nvim/config/lua/user/plugins/editing.lua
+++ nvim/config/lua/user/plugins/editing.lua
@@ -4,7 +4,24 @@ local M = {
     'tpope/vim-sleuth',
 
     -- Useful plugin to show pending keybinds.
-    { 'folke/which-key.nvim', event = 'VeryLazy', opts = {} },
+    {
+        'folke/which-key.nvim',
+        event = 'VeryLazy',
+        opts = {},
+        config = function(_, opts)
+            require('which-key').setup(opts)
+
+            -- Document existing key chains
+            require('which-key').register {
+                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+                ['<leader>d'] = { name = '[D]AP', _ = 'which_key_ignore' },
+                -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+                ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+            }
+        end
+    },
 
     {
         -- Add indentation guides even on blank lines
@@ -66,8 +83,6 @@ local M = {
             -- ignore = nil,
             toggler = { line = 'gcc', block = 'gbc' },
             opleader = { line = 'gc', block = 'gb' },
-            -- toggler = { line = '<leader>cc', block = 'gbc' },
-            -- opleader = { line = '<leader>c', block = 'gb' },
             -- extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
             -- mappings = { basic = true, extra = true },
             -- pre_hook = nil,
@@ -122,71 +137,14 @@ local M = {
     {
         'folke/todo-comments.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {
-            -- DEFAULTS:
-            -- signs = true,          -- show icons in the signs column
-            -- sign_priority = 8,     -- sign priority
-            -- -- keywords recognized as todo comments
-            -- keywords = {
-            --     FIX = {
-            --         icon = " ", -- icon used for the sign, and in search results
-            --         color = "error", -- can be a hex color, or a named color (see below)
-            --         alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-            --         -- signs = false, -- configure signs for some keywords individually
-            --     },
-            --     TODO = { icon = " ", color = "info" },
-            --     HACK = { icon = " ", color = "warning" },
-            --     WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-            --     PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-            --     NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-            --     TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-            -- },
-            -- gui_style = {
-            --     fg = "NONE",           -- The gui style to use for the fg highlight group.
-            --     bg = "BOLD",           -- The gui style to use for the bg highlight group.
-            -- },
-            -- merge_keywords = true,     -- when true, custom keywords will be merged with the defaults
-            -- -- highlighting of the line containing the todo comment
-            -- -- * before: highlights before the keyword (typically comment characters)
-            -- -- * keyword: highlights of the keyword
-            -- -- * after: highlights after the keyword (todo text)
-            -- highlight = {
-            --     multiline = true,                    -- enable multine todo comments
-            --     multiline_pattern = "^.",            -- lua pattern to match the next multiline from the start of the matched keyword
-            --     multiline_context = 10,              -- extra lines that will be re-evaluated when changing a line
-            --     before = "",                         -- "fg" or "bg" or empty
-            --     keyword = "wide",                    -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-            --     after = "fg",                        -- "fg" or "bg" or empty
-            --     pattern = [[.*<(KEYWORDS)\s*:]],     -- pattern or table of patterns, used for highlighting (vim regex)
-            --     comments_only = true,                -- uses treesitter to match keywords in comments only
-            --     max_line_len = 400,                  -- ignore lines longer than this
-            --     exclude = {},                        -- list of file types to exclude highlighting
-            -- },
-            -- -- list of named colors where we try to extract the guifg from the
-            -- -- list of highlight groups or use the hex color if hl not found as a fallback
-            -- colors = {
-            --     error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-            --     warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-            --     info = { "DiagnosticInfo", "#2563EB" },
-            --     hint = { "DiagnosticHint", "#10B981" },
-            --     default = { "Identifier", "#7C3AED" },
-            --     test = { "Identifier", "#FF00FF" }
-            -- },
-            -- search = {
-            --     command = "rg",
-            --     args = {
-            --         "--color=never",
-            --         "--no-heading",
-            --         "--with-filename",
-            --         "--line-number",
-            --         "--column",
-            --     },
-            --     -- regex that will be used to match keywords.
-            --     -- don't replace the (KEYWORDS) placeholder
-            --     pattern = [[\b(KEYWORDS):]],     -- ripgrep regex
-            --     -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-            -- },
+        -- :h todo-comments.nvim.txt
+        opts = {},
+
+        keys = {
+            { "<leader>st", function() vim.cmd.TodoTelescope() end, desc = "[S]earch [T]odos" },
         },
+
+        cmd = { 'TodoQuickfix', 'TodoTelescope' },
     },
 }
 
