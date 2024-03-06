19ced0bdf902871bbbc3cdc140b6eed9c9e7347b
Louis Brunet <louis.brunet@neoxia.com>
2024-03-06 23:30:04 +0100

    nvim: refactor nvim-rest config to be more idiomatic Lazy

--- nvim/config/lua/user/plugins/rest.lua
+++ nvim/config/lua/user/plugins/rest.lua
@@ -4,80 +4,93 @@ return {
     dependencies = {
         { "nvim-lua/plenary.nvim" },
     },
-    config = function()
 
+    ft = { 'http' },
+
+    keys = {
+        { '<leader>rq', function() require('rest-nvim').run() end, buffer = true, desc = '[R]estNvim re[q]uest' },
+        { '<leader>rp', '<Plug>RestNvimPreview',                   buffer = true, desc = '[R]estNvim [p]review' },
+    },
+
+    opts = {
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+
+        -- stay in current windows (.http file) or change to results window (default)
+        stay_in_current_window_after_split = false,
+
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+
+        -- Encode URL before making request
+        encode_url = true,
+
+        -- Highlight request on run
+        highlight = {
+            enabled = true,
+            timeout = 150,
+        },
+
+        result = {
+            -- toggle showing URL, HTTP info, headers at top the of result window
+            show_url = true,
+
+            -- show the generated curl command in case you want to launch
+            -- the same request via the terminal (can be verbose)
+            show_curl_command = true,
+
+            show_http_info = true,
+
+            show_headers = true,
+
+            -- table of curl `--write-out` variables or false if disabled
+            -- for more granular control see Statistics Spec
+            show_statistics = false,
+
+            -- executables or functions for formatting response body [optional]
+            -- set them to false if you want to disable them
+            formatters = {
+                -- json = "jq",
+                -- html = function(body)
+                --     return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+                -- end
+            },
+        },
+
+        -- Jump to request line on run
+        jump_to_request = false,
+
+        env_file = '.env',
+
+        -- for telescope select
+        env_pattern = "\\.env$",
+
+        env_edit_command = "tabedit",
+
+        custom_dynamic_variables = {},
+
+        yank_dry_run = true,
+
+        search_back = true,
+    },
+
+    config = function(_, opts)
         -- available keymaps (rhs of vim.keymap.set(mode, lhs, rhs, ...)):
         --   <Plug>RestNvim         : run HTTP request under cursor (in .http file)
+        --      (same as require(rest-nvim').run())
         --   <Plug>RestNvimPreview  : preview underlying cURL command
         --   <Plug>RestNvimLast     : rerun last HTTP request
-        --
-        -- NOTE: see after/ftplugin/http.lua for filetype-specific mappings on
-        -- filetype 'http'.
 
         local rest_nvim = require('rest-nvim')
-        rest_nvim.setup({
-            -- Open request results in a horizontal split
-            result_split_horizontal = false,
+        local wk = require('which-key')
 
-            -- Keep the http file buffer above|left when split horizontal|vertical
-            result_split_in_place = false,
-
-            -- stay in current windows (.http file) or change to results window (default)
-            stay_in_current_window_after_split = false,
-
-            -- Skip SSL verification, useful for unknown certificates
-            skip_ssl_verification = false,
-
-            -- Encode URL before making request
-            encode_url = true,
-
-            -- Highlight request on run
-            highlight = {
-                enabled = true,
-                timeout = 150,
-            },
-
-            result = {
-                -- toggle showing URL, HTTP info, headers at top the of result window
-                show_url = true,
-
-                -- show the generated curl command in case you want to launch
-                -- the same request via the terminal (can be verbose)
-                show_curl_command = true,
-
-                show_http_info = true,
-
-                show_headers = true,
-
-                -- table of curl `--write-out` variables or false if disabled
-                -- for more granular control see Statistics Spec
-                show_statistics = false,
-
-                -- executables or functions for formatting response body [optional]
-                -- set them to false if you want to disable them
-                formatters = {
-                    -- json = "jq",
-                    -- html = function(body)
-                    --     return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-                    -- end
-                },
-            },
-
-            -- Jump to request line on run
-            jump_to_request = false,
-
-            env_file = '.env',
-
-            -- for telescope select
-            env_pattern = "\\.env$",
-
-            env_edit_command = "tabedit",
-
-            custom_dynamic_variables = {},
-
-            yank_dry_run = true,
-
-            search_back = true,
+        wk.register({
+            ['<leader>r'] = { name = '+[r]est-nvim', _ = 'which_key_ignore' },
         })
-    end
+
+        rest_nvim.setup(opts)
+    end,
 }
