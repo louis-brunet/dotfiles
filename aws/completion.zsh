89bd297bba4b76b301af5e09aac1e38044b0af06
Louis Brunet <louis.brunet@neoxia.com>
2024-03-06 17:26:02 +0100

    aws: add shell completion for cdktf cli

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -1,2 +1,27 @@
+# aws
 complete -C '/usr/local/bin/aws_completer' aws
+
+
+# terraform
 complete -o nospace -C '/usr/bin/terraform' terraform
+
+
+#compdef cdktf
+###-begin-cdktf-completions-###
+#
+# yargs command completion script
+#
+# Installation: cdktf completion >> ~/.zshrc
+#    or cdktf completion >> ~/.zsh_profile on OSX.
+#
+_cdktf_yargs_completions()
+{
+  local reply
+  local si=$IFS
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" cdktf --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  _describe 'values' reply
+}
+compdef _cdktf_yargs_completions cdktf
+###-end-cdktf-completions-###
