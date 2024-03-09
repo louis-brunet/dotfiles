d4320701ce2eafeb74ecf6468645540a721ad8e7
Louis Brunet <louis.brunet@neoxia.com>
2024-03-09 14:25:32 +0100

    zsh: don't add to PATH if already present (e.g. after `exec zsh`)

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,12 @@
-export PATH="$HOME/bin:$DOTFILES/bin:$PATH"
+local system_bin="$HOME/bin:$DOTFILES/bin"
+
+case ":${PATH}:" in
+    *:"$system_bin":*)
+        ;;
+    *)
+        export PATH="$system_bin:$PATH"
+        ;;
+esac
+# export PATH="$HOME/bin:$DOTFILES/bin:$PATH"
 
 export XDG_CONFIG_HOME="$HOME/.config"
-
--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,5 +1,5 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($DOTFILES/*) [ -d $topic_folder ] && fpath=($topic_folder $fpath);
+# #add each topic folder to fpath so that they can add functions and completion scripts
+# for topic_folder ($DOTFILES/*) [ -d $topic_folder ] && fpath=($topic_folder $fpath);
 
 fpath=($HOME/bin $fpath)
 
