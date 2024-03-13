8f974e1fa329d78bbb78864add0d9074b0c3f376
Louis Brunet <louis.brunet@neoxia.com>
2024-03-13 19:54:00 +0100

    fix(rust): remove call to rustup, cargo, rustc if they were just installed
    Might need `exec zsh` for changes to take effect.

--- rust/install.sh
+++ rust/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -e
 
@@ -8,13 +8,14 @@ ANSI_RESET="\033[0m"
 
 if ! which rustup; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+    echo "✅ installed rust toolchain (might need to 'exec zsh')"
+else
+    rustup --version
+    echo "✅ rustup is installed"
+
+    cargo --version
+    echo "✅ cargo is installed"
+
+    rustc --version
+    echo "✅ rustc is installed"
 fi
-
-rustup --version
-echo "✅ rustup is installed"
-
-cargo --version
-echo "✅ cargo is installed"
-
-rustc --version
-echo "✅ rustc is installed"
