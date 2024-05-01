55596c8989e01f42a6b916a29a394b397573f77f
Louis Brunet <123louisb@gmail.com>
2024-05-01 22:52:41 +0200

    refactor(nvim): create utils function ui_confirm

--- nvim/config/ftplugin/qf.lua
+++ nvim/config/ftplugin/qf.lua
@@ -2,26 +2,15 @@ vim.keymap.set(
     'n',
     '<leader>qd',
     function()
-        local current_line = vim.api.nvim_win_get_cursor(0)[1]
-        local CHOICES = { yes = 'Yes', no = 'No' }
-
-        vim.ui.select(
-            { CHOICES.no, CHOICES.yes },
-            {
-                prompts = 'Delete quickfix item at line ' .. current_line .. '?',
-                -- format_item = function(item) return 'toto'..item end,
-            },
-            function(chosen_item)
-                if chosen_item == CHOICES.yes then
-                    require('user.utils.quickfix').delete_quickfix_item(current_line)
-                    vim.notify('Deleted quickfix item', vim.log.levels.INFO, { title = '' })
-                elseif chosen_item == CHOICES.no then
-
-                else
-                    error('todo')
-                end
-            end
-        )
+        require('user.utils.quickfix').delete_quickfix_current_line({ confirm = false })
     end,
-    { desc = '', buffer = 0 }
+    { desc = '[q]uickfix: [d]elete current line', buffer = 0 }
+)
+vim.keymap.set(
+    'x',
+    '<leader>qd',
+    function()
+        require('user.utils.quickfix').delete_quickfix_visual({ confirm = false })
+    end,
+    { desc = '[q]uickfix: [d]elete selected lines', buffer = 0 }
 )
