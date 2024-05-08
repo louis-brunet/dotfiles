94ddccb246cde7f4426dea93b369744d1828e312
Louis Brunet <123louisb@gmail.com>
2024-05-08 02:53:52 +0200

    feat(system): add xdg base dirs

--- system/path.zsh
+++ system/path.zsh
@@ -9,4 +9,8 @@ case ":${PATH}:" in
 esac
 # export PATH="$HOME/bin:$DOTFILES/bin:$PATH"
 
+# https://wiki.archlinux.org/title/XDG_Base_Directory
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
