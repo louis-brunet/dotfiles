9cda1c2fed6861dc04aad8b1ff9cc2632c2eda78
Louis Brunet <louis.brunet1@grenoble-inp.org>
2024-08-06 11:06:42 +0200

    chore: comment out unused code

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,11 +3,15 @@
 set -e
 
 sudo apt install tmux -y
+
 tmux -V
 
 if ! which fzf
 then
     sudo apt install fzf -y
+    # git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    # ~/.fzf/install --no-update-rc --xdg
+
 fi
 fzf --version
 
