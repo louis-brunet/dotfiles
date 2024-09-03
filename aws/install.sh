e5da8959d4898563f5ede9caa2d9f2e80b8336ff
Louis Brunet <123louisb@gmail.com>
2024-09-03 19:05:42 +0200

    fix: install terraform with snap

--- aws/install.sh
+++ aws/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ "$LOCAL_ENV" != "neoxia" ]]
-then
-    exit 0
-fi
+# if [[ "$LOCAL_ENV" != "neoxia" ]]
+# then
+#     exit 0
+# fi
 
 set -e
 set -x
@@ -37,33 +37,23 @@ fi
 
 # Install Terraform CLI
 if ! which terraform >/dev/null; then
-    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+    echo "Installing Terraform CLI"
 
-    wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+    sudo snap install --classic terraform
 
-    gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-    sudo apt update
-
-    sudo apt-get install terraform
+    # wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    # echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    # sudo apt update && sudo apt install terraform
 
     terraform -help
 
     echo "✅ installed terraform"
 fi
 
-# Install CDK for Terraform
-if ! which cdktf >/dev/null; then
-    npm install --global cdktf-cli@latest
-    cdktf --version
-    echo "✅ installed cdktf (CDK for Terraform)"
-fi
+# # Install CDK for Terraform
+# if ! which cdktf >/dev/null; then
+#     npm install --global cdktf-cli@latest
+#     cdktf --version
+#     echo "✅ installed cdktf (CDK for Terraform)"
+# fi
 
