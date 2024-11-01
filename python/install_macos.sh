a0ef5d03b621c8345c7192c377a46050a9d0f90f
Louis Brunet <lbrunet@neoxia.deloitte.fr>
2024-11-01 19:14:51 +0100

    docs(python): add link to poetry installation docs

--- python/install_macos.sh
+++ python/install_macos.sh
@@ -13,6 +13,7 @@ brew install pyenv
 }
 
 which poetry || {
+    # https://python-poetry.org/docs/#installing-with-the-official-installer
     curl -sSL https://install.python-poetry.org | python3 -
 }
 
