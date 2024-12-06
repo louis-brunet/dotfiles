1d53077a5d7e6800f7418318a656cb7033f10110
Louis Brunet <lbrunet@neoxia.deloitte.fr>
2024-12-06 17:59:46 +0100

    fix(python): add commented code to fix Netskope issues with REQUESTS_CA_BUNDLE

--- python/path.zsh
+++ python/path.zsh
@@ -14,3 +14,19 @@ eval "$(pyenv virtualenv-init -)"
 # links to
 # https://github.com/tox-dev/platformdirs/issues/4
 export POETRY_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/pypoetry"
+
+# if [ "$LOCAL_ENV" = "neoxia" ]; then
+#     # NOTE: will not work when not on macOS
+#     if which security >/dev/null; then
+#         combined_netskope_cert="/Library/Application Support/Netskope/STAgent/data/nscacert_combined.pem"
+#         if [ ! -f "$combined_netskope_cert" ]; then
+#             echo "missing file '$combined_netskope_cert', generating"
+#             tmp_file="$(mktemp)"
+#
+#             security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain /Library/Keychains/System.keychain >"$tmp_file"
+#             sudo cp "$tmp_file" "$combined_netskope_cert"
+#         fi
+#         export REQUESTS_CA_BUNDLE="$combined_netskope_cert"
+#     fi
+#     # export REQUESTS_CA_BUNDLE="/Library/Application Support/Netskope/STAgent/data/nscacert.pem"
+# fi
