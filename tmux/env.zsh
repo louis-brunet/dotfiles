7992ad17664abfcba641dd20b6b529b9118c0bcf
Louis Brunet <lbrunet@neoxia.deloitte.fr>
2024-12-07 19:13:04 +0100

    fix(tmux): use custom history widget, install fzf with brew on macOS

--- tmux/env.zsh
+++
@@ -1,8 +0,0 @@
-export TMUX_SESSIONIZER_DIRS="$PROJECTS"
-
-# possibly defined in ~/.localrc
-if [[ -n "$LOCAL_TMUX_SESSIONIZER_DIRS" ]]; then
-    export TMUX_SESSIONIZER_DIRS="$LOCAL_TMUX_SESSIONIZER_DIRS:$TMUX_SESSIONIZER_DIRS"
-fi
-
-# vi:ft=sh:
