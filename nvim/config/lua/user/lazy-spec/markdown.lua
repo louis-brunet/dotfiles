cc6b37c0ce2d88052d2976dd6a601695e4e89e64
Louis Brunet <lbrunet@neoxia.deloitte.fr>
2025-01-23 16:18:03 +0100

    feat(nvim): remove table top and bottom borders in render-markdown, add automatic table formatting

--- nvim/config/lua/user/lazy-spec/markdown.lua
+++ nvim/config/lua/user/lazy-spec/markdown.lua
@@ -2,10 +2,37 @@ local render_markdown_filetypes = { "markdown", "Avante" }
 
 ---@type LazySpec
 return {
-    -- Make sure to set this up properly if you have lazy=true
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {
-        file_types = render_markdown_filetypes,
+    {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+            file_types = render_markdown_filetypes,
+            pipe_table = { style = "normal", preset = "double" },
+        },
+        ft = render_markdown_filetypes,
+        -- dependencies = {'hrsh7th/nvim-cmp'},
+        config = function(_, opts)
+            require("render-markdown").setup(opts)
+            -- local cmp = require('cmp')
+            -- cmp.setup({
+            --     sources = cmp.config.sources({
+            --         { name = 'render-markdown' },
+            --     }),
+            -- })
+        end,
     },
-    ft = render_markdown_filetypes,
+    {
+        -- Auto-magically format markdown tables under the cursor
+        "Kicamon/markdown-table-mode.nvim",
+        ft = render_markdown_filetypes,
+        opts = {
+            -- see https://github.com/Kicamon/markdown-table-mode.nvim?tab=readme-ov-file#configuration
+            -- filetype = {},
+            options = {
+                insert = true,        -- when typing "|"
+                insert_leave = true,  -- when leaving insert
+            },
+        },
+        config = true,
+    },
 }
