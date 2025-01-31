abc1cd84a5244f5945b05765687ec01496ccddf8
Louis Brunet <lbrunet@neoxia.deloitte.fr>
2025-01-31 13:49:33 +0100

    refactor(nvim): clean up custom user commands for llama.vim (toggle and show completions status)

--- nvim/config/lua/user/lazy-spec/ai/llama_vim.lua
+++ nvim/config/lua/user/lazy-spec/ai/llama_vim.lua
@@ -1,17 +1,55 @@
+local llama_server_origin = "http://127.0.0.1:8012"
+local llama_endpoints = {
+    enabled = llama_server_origin .. "/infill",
+    disabled = "__DISABLED__"
+}
+
+local notify = function(message)
+    vim.notify(
+        message,
+        vim.log.levels.INFO,
+        { title = "llama.vim" }
+    )
+end
+
+local function are_llm_completions_enabled()
+    local opts = vim.g.llama_config
+    return opts.endpoint == llama_endpoints.enabled
+end
+
+local function notify_llm_completions_status()
+    local message = "LLM completions are "
+
+    if are_llm_completions_enabled() then
+        message = message .. "enabled"
+    else
+        message = message .. "disabled"
+    end
+
+    notify(message)
+end
+
+local function toggle_llm_completions()
+    local opts = vim.g.llama_config
+    if opts.endpoint ~= llama_endpoints.disabled then
+        opts.endpoint = llama_endpoints.disabled
+    else
+        opts.endpoint = llama_endpoints.enabled
+    end
+    vim.g.llama_config = opts
+
+    notify_llm_completions_status()
+end
+
 ---@type LazySpec
 local M = {
     {
         "louis-brunet/llama.vim",
 
-        -- dependencies = {
-        --     'projekt0n/github-nvim-theme'
-        -- },
-
-
         init = function()
             -- NOTE: see :help llama_config
             vim.g.llama_config = {
-                -- endpoint = "http://127.0.0.1:8012/infill",
+                endpoint = llama_endpoints.enabled,
                 -- api_key = "",
                 -- n_prefix = 256,
                 -- n_suffix = 64,
@@ -19,7 +57,7 @@ local M = {
                 -- t_max_prompt_ms = 500,
                 -- t_max_predict_ms = 500,
                 -- show_info = 2,
-                auto_fim = true,
+                -- auto_fim = true,
                 -- max_line_suffix = 8,
                 -- max_cache_keys = 250,
                 -- ring_n_chunks = 16,
@@ -27,44 +65,21 @@ local M = {
                 -- ring_scope = 1024,
                 -- ring_update_ms = 1000,
             }
-
             -- vim.fn.call("llama#init", {})
 
             -- NOTE: customized llama.vim highlight llama_hf_info and
             -- llama_hg_hint; see nvim/config/lua/user/lazy-spec/theme.lua
 
-            local notify = function(message)
-                vim.notify(
-                    message,
-                    vim.log.levels.INFO,
-                    { title = "llama.vim" }
-                )
-            end
-
-            local function toggle_llm_completions()
-                local opts = vim.g.llama_config
-                if opts.endpoint ~= "" then
-                    opts.endpoint = ""
-                else
-                    opts.endpoint = "http://127.0.0.1:8012/infill"
-                end
-                vim.g.llama_config = opts
-
-                local is_enabled = opts.endpoint ~= ""
-                local message = "LLM completions are "
-                if is_enabled then
-                    message = message .. "enabled"
-                else
-                    message = message .. "disabled"
-                end
-
-                notify(message)
-            end
+            vim.api.nvim_create_user_command(
+                "LlmCompletionsToggle",
+                toggle_llm_completions,
+                { desc = "llama.vim: toggle completions" }
+            )
 
             vim.api.nvim_create_user_command(
-                "ToggleCompletions",
-                toggle_llm_completions,
-                { desc = "Toggle llama.vim completions" }
+                "LlmCompletionsStatus",
+                notify_llm_completions_status,
+                { desc = "llama.vim: show completions status" }
             )
         end,
     },
