aaa8e59f23fe11f820dab5049792d8dda0cda7fd
Louis Brunet <louis.jc.brunet@gmail.com>
2025-03-14 21:48:47 +0100

    fix(tmux): fix fzf installation and PATH

--- tmux/path.zsh
+++ tmux/path.zsh
@@ -1,21 +1,4 @@
-_configure_fzf_path() {
-    if ! which fzf 2>&1 >/dev/null; then
-        local manual_fzf_install_path="$HOME/.fzf/bin"
-        if [ -e "$manual_fzf_install_path/fzf" ] && [ ! "$PATH" == *"$manual_fzf_install_path"* ]; then
-            export PATH="$manual_fzf_install_path:$PATH"
-        fi
-    fi
-}
-
-_configure_fzf_path
-
-# if [ -d "$HOME/.fzf/bin" ]; then
-#     ...
-# fi
-
-# if [[ ! "$PATH" == */Users/lbrunet/.fzf/bin* ]]; then
-#   PATH="${PATH:+${PATH}:}/Users/lbrunet/.fzf/bin"
-# fi
+[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
 
 export TMUX_SESSIONIZER_DIRS="$PROJECTS"
 
@@ -24,4 +7,4 @@ if [[ -n "$LOCAL_TMUX_SESSIONIZER_DIRS" ]]; then
     export TMUX_SESSIONIZER_DIRS="$LOCAL_TMUX_SESSIONIZER_DIRS:$TMUX_SESSIONIZER_DIRS"
 fi
 
-# vi:ft=sh:
+# vim:ft=sh:
