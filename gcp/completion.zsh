0618e89beb4bc55225668b505bc4419262e1884a
Louis Brunet <lbrunet@deloitte.fr>
2025-03-19 17:44:03 +0100

    fix(gcp): fix completions for homebrew install

--- gcp/completion.zsh
+++ gcp/completion.zsh
@@ -1,6 +1,15 @@
 if [ "$LOCAL_ENV" = "neoxia" ]; then
-    gcloud_completion=/usr/share/google-cloud-sdk/completion.zsh.inc
-    if [ -f "$gcloud_completion" ]; then
-        source "$gcloud_completion"
-    fi
+    gcloud_search_locations=(
+        "$HOME/google-cloud-sdk"
+        "/opt/homebrew/share/google-cloud-sdk"
+        "/usr/share/google-cloud-sdk"
+    )
+
+    for gcloud_sdk_location in $gcloud_search_locations; do
+        gcloud_completion="$gcloud_sdk_location"/completion.zsh.inc
+        if [[ -f "${gcloud_completion}" ]]; then
+            source "$gcloud_completion"
+            break
+        fi
+    done
 fi
