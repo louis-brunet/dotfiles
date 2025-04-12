23f4add46b3ce62fe47eb56a18d813752a7c8363
Louis Brunet <louis.jc.brunet@gmail.com>
2025-04-12 12:25:26 +0200

    fix(nvim/install): update download URLs, checksum validation for v0.11

--- nvim/install.sh
+++ nvim/install.sh
@@ -12,21 +12,25 @@ if [[ ! -x "$nvim_path" ]] || [[ -n "$DOTFILES_ALWAYS_UPDATE_NVIM" ]]; then
     # download latest stable appimage, save it as $HOME/bin/nvim with execution permissions
     echo "🌐 pulling latest stable nvim appimage"
 
+    nvim_release_url="https://github.com/neovim/neovim/releases/download/stable"
     nvim_download_path="${nvim_path}.downloading"
-    nvim_download_url="https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage"
-    nvim_sha_download_path="${nvim_path}.sha256sum"
-    nvim_sha_download_url="${nvim_download_url}.sha256sum"
+    nvim_download_url="${nvim_release_url}/nvim-linux-x86_64.appimage"
+    nvim_shasum_txt_download_path="${nvim_path}.shasum.txt"
+    nvim_shasum_txt_download_url="${nvim_release_url}/shasum.txt"
 
     mkdir -p "$(dirname "$nvim_path")"
 
     curl -o "$nvim_download_path" -L "$nvim_download_url"
-    curl -o "${nvim_sha_download_path}" -L "$nvim_sha_download_url"
+    curl -o "$nvim_shasum_txt_download_path" -L "$nvim_shasum_txt_download_url"
 
-    sha256sum "$nvim_download_path" | grep "$(cat "$nvim_sha_download_path" | cut -f 1 -d ' ')" || {
+    nvim_download_path_sha256=$(sha256sum "$nvim_download_path" | awk '{print $1}')
+    grep --quiet "$nvim_download_path_sha256" "$nvim_shasum_txt_download_path" || {
         echo "invalid checksum for ${nvim_download_path}"
+        rm "$nvim_shasum_txt_download_path"
         exit 1
     }
     echo 'Validated checksum'
+    rm "$nvim_shasum_txt_download_path"
 
     mv "$nvim_download_path" "$nvim_path"
     chmod u+x "$nvim_path"
@@ -57,7 +61,7 @@ fi
 # sudo apt install -y liblua5.1-0-dev
 # echo "✅ installed Lua for luarocks.nvim (rest.nvim)"
 
-# NOTE: install missing python3-venv for Mason to install certain packages, 
+# NOTE: install missing python3-venv for Mason to install certain packages,
 # like ruff. Should only be needed for Ubuntu/Debian
 # WARN: assumes that WSL is running Ubuntu/Debian
 if python3 -mplatform | grep -qiE 'Ubuntu|Debian|WSL'; then
