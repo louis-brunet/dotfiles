bff5eb1d5bb5524df6016b67bc4b1c3bdacf7ff5
Louis Brunet <louis.jc.brunet@gmail.com>
2025-04-13 19:35:46 +0200

    feat(nvim): prompt user to add file after git conflict resolution

--- nvim/config/lua/user/lazy-spec/git.lua
+++ nvim/config/lua/user/lazy-spec/git.lua
@@ -1,52 +1,3 @@
-local merge_conflict_pattern =
-[[/<<<<.*\n\(\(====\)\@!.*\n\)*====.*\n\(\(>>>>\)\@!.*\n\)*>>>>.*$/]]
-
--- ---@param flags { nojump: boolean|nil, global: boolean|nil, fuzzy: boolean|nil } | nil
--- ---@return string
--- local function merge_conflict_pattern_with_flags(flags)
---     flags = flags or {}
---     local flags_str = ''
---
---     if flags.fuzzy then flags_str = flags_str .. 'f' end
---     if flags.global then flags_str = flags_str .. 'g' end
---     if flags.nojump then flags_str = flags_str .. 'j' end
---
---     return merge_conflict_pattern .. flags_str
--- end
-
----@param opts { filenames: string|nil, loclist: boolean|nil, open: boolean|nil }|nil
-local function list_merge_conflicts(opts)
-    opts = opts or {}
-    opts.filenames = opts.filenames or "%"
-    local pattern = merge_conflict_pattern
-
-    local list_cmd = "vimgrep"
-    local open_cmd = "copen"
-    if opts.loclist then
-        list_cmd = "lvimgrep"
-        open_cmd = "lopen"
-    end
-
-    local ok, _ = pcall(
-        vim.api.nvim_cmd,
-        {
-            cmd = list_cmd,
-            args = { pattern, opts.filenames },
-            mods = { silent = true },
-        },
-        {}
-    )
-    if not ok then
-        vim.notify("[list_merge_conflicts]  no merge conflicts",
-            vim.log.levels.INFO)
-        return
-    end
-
-    if opts.open then
-        vim.cmd(open_cmd)
-    end
-end
-
 --- [ Git related plugins ]
 ---@type LazySpec
 local M = {
@@ -131,51 +82,182 @@ local M = {
                     function() require("gitsigns").preview_hunk_inline() end,
                     "[g]it: Preview [h]unk [d]iff"
                 )
-
-                -- Other mappings only used in a git buffer
-                -- TODO: more generic diffget keybinds, the handlers should check:
-                --  1. how many buffers ?
-                --  2. which layout ? (why different in desktop ~/code/test/mergeconflict vs neoxia ~/code/test/merge*_nobase ?)
-                --
-                -- assumes nvimdiff3 layout (LOCAL BASE REMOTE / MERGED), or (LOCAL MERGED REMOTE)
-                nmap(
-                    "<leader>gmh",
-                    function() vim.cmd.diffget(vim.fn.tabpagebuflist()[1]) end,
-                    "[g]it [m]erge diffget left (LOCAL) "
-                )
-                nmap(
-                    "<leader>gmk",
-                    function() vim.cmd.diffget(vim.fn.tabpagebuflist()[2]) end,
-                    "[g]it [m]erge diffget middle (BASE)"
-                )
-                nmap(
-                    "<leader>gml",
-                    function() vim.cmd.diffget(vim.fn.tabpagebuflist()[3]) end,
-                    "[g]it [m]erge diffget right (REMOTE)"
-                )
-
-                nmap(
-                    "<leader>gmc",
-                    function() list_merge_conflicts({ open = true }) end,
-                    "[g]it [m]erge [c]onflicts quickfix"
-                )
             end,
         },
     },
 
     {
         "akinsho/git-conflict.nvim",
+        -- event = "VeryLazy", NOTE: plugin seems broken when loaded on VeryLazy
         -- https://github.com/akinsho/git-conflict.nvim?tab=readme-ov-file#configuration
+        ---@type GitConflictUserConfig
         opts = {
-            -- default_mappings = true,  -- disable buffer local mapping created by this plugin
+            default_mappings = false,    -- disable buffer local mapping created by this plugin
             -- default_commands = true,  -- disable commands created by this plugin
             disable_diagnostics = true,  -- This will disable the diagnostics in a buffer whilst it is conflicted
             -- list_opener = "copen",    -- command or function to open the conflicts list
-            -- highlights = {            -- They must have background color, otherwise the default color will be used
-            --     incoming = "DiffAdd",
-            --     current = "DiffText",
-            -- },
+            highlights = {
+                -- incoming = "DiffAdd",
+                -- current = "DiffText",
+                incoming = "DiffChange",
+                current = "DiffAdd",
+                ancestor = "DiffDelete",
+            },
         },
+        config = function(self, opts)
+            local git_conflict = require("git-conflict")
+            git_conflict.setup(opts)
+
+            ---@param lhs string[]|string
+            ---@param rhs string|function
+            ---@param description? string|nil
+            local function nmap(lhs, rhs, description)
+                description = description or
+                    ((type(rhs) == "string" and rhs) or nil)
+                if type(description) == "string" then
+                    description = "conflict: " .. description
+                end
+                if type(lhs) == "string" then
+                    lhs = { lhs }
+                end
+                for _, lhs_item in ipairs(lhs) do
+                    vim.keymap.set("n", lhs_item, rhs, { desc = description })
+                end
+            end
+
+            nmap({ "co", "<leader>gmh" }, "<Plug>(git-conflict-ours)",
+                "choose ours")
+            nmap({ "ct", "<leader>gml" }, "<Plug>(git-conflict-theirs)",
+                "choose theirs")
+            nmap({ "cb", "<leader>gmb" }, "<Plug>(git-conflict-base)",
+                "choose base")
+            nmap({ "cb", "<leader>gmB" }, "<Plug>(git-conflict-both)",
+                "choose both")
+            nmap({ "c0", "<leader>gm0" }, "<Plug>(git-conflict-none)",
+                "choose none")
+            nmap({ "[x", "<leader>gmn" }, "<Plug>(git-conflict-prev-conflict)",
+                "previous conflict")
+            nmap({ "]x", "<leader>gmp" }, "<Plug>(git-conflict-next-conflict)",
+                "next conflict")
+            nmap("<leader>gmq", function()
+                vim.cmd.cexpr("[]")
+                -- vim.cmd("GitConflictRefresh")
+                vim.cmd("GitConflictListQf")
+            end, "send to quickfix")
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "GitConflictDetected",
+                callback = function()
+                    local bufnr = 0
+                    local conflict_count = git_conflict.conflict_count(bufnr)
+                    local message = ("Conflict detected in %s (%d conflicts)")
+                        :format(vim.fn.expand("%"), conflict_count)
+                    vim.schedule(function()
+                        vim.notify(message, vim.log.levels.WARN)
+                    end)
+                end,
+                group = vim.api.nvim_create_augroup(
+                    "GitConflictDetected_augroup",
+                    { clear = true }),
+            })
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "GitConflictResolved",
+                callback = function()
+                    local git_utils = require("user.utils.git")
+                    local conflict_file = vim.fn.expand("%")
+                    local message = ("Conflict resolved in %s")
+                        :format(conflict_file)
+                    vim.schedule(function()
+                        vim.notify(message)
+
+                        if not git_utils.is_unmerged_file(conflict_file) then
+                            -- vim.notify(
+                            --     "[DEBUG] aborting, file is already merged")
+                            return
+                        end
+
+                        -- local choice_list = {
+                        --     {
+                        --         label = "Cancel",
+                        --         on_select = function()
+                        --         end,
+                        --     },
+                        --     {
+                        --         label = "Add entire file",
+                        --         on_select = function()
+                        --             local is_written = pcall(vim.cmd.write)
+                        --             if not is_written then
+                        --                 vim.notify(
+                        --                     "aborting git add, could not write to file: " ..
+                        --                     conflict_file, vim.log.levels.ERROR)
+                        --                 return
+                        --             end
+                        --             if git_utils.add_file(conflict_file) then
+                        --                 vim.notify("added file to git index: " ..
+                        --                     conflict_file)
+                        --             end
+                        --         end,
+                        --     },
+                        --     -- {
+                        --     --     -- value = "interactive",
+                        --     --     label = "Interactive",
+                        --     --     on_select = function ()
+                        --     --         vim.notify('TODO interactive add', vim.log.levels.ERROR)
+                        --     --     end,
+                        --     -- },
+                        -- }
+                        -- local prompt =
+                        --     ("All conflicts resolved! Add to git index? (%s)")
+                        --     :format(conflict_file)
+                        -- vim.ui.select(choice_list,
+                        --     {
+                        --         prompt = prompt,
+                        --         format_item =
+                        --             function(item) return item.label end,
+                        --     },
+                        --     function(chosen_item)
+                        --         if chosen_item == nil then
+                        --             return
+                        --         end
+                        --         vim.notify("chose item: " ..
+                        --             vim.inspect(chosen_item))
+                        --         chosen_item.on_select()
+                        --     end)
+
+                        local ui = require("user.utils.ui")
+                        ui.ui_confirm(
+                            ("All conflicts resolved! Add to git index? (%s)")
+                            :format(conflict_file),
+                            {
+                                on_accept = function()
+                                    local is_written = pcall(vim.cmd.write)
+                                    if not is_written then
+                                        vim.notify(
+                                            "aborting git add, could not write to file: " ..
+                                            conflict_file, vim.log.levels.ERROR)
+                                        return
+                                    end
+                                    if git_utils.add_file(conflict_file) then
+                                        vim.notify("added file to git index: " ..
+                                            conflict_file)
+                                    end
+                                end,
+                                on_deny = function()
+                                    vim.notify("denied git add")
+                                end,
+                            },
+                            {
+                                choices = { yes = "Add entire file", no = "Cancel", },
+                            }
+                        )
+                    end)
+                end,
+                group = vim.api.nvim_create_augroup(
+                    "GitConflictResolved_augroup",
+                    { clear = true }),
+            })
+        end,
     },
 }
 
