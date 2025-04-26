12674e6229ce397d227c9efd371c62a672ef2b4a
Louis Brunet <louis.jc.brunet@gmail.com>
2025-04-26 13:48:35 +0200

    feat(mann): open table of contents when opening man page in nvim

--- nvim/functions.zsh
+++ nvim/functions.zsh
@@ -1,7 +1,16 @@
+# NOTE: completions for this function are defined in ../functions/_mann because
+# ../functions is in `$fpath`
 mann() {
     if [[ $# -lt 1 ]]; then
         echo "usage: $0 <man_args...>" >&2
         return 1
     fi
-    nvim +"Man $*" +"wincmd o"
+    # Open the man page, close other windows, open table of contents on the
+    # right side, put cursor back in man page.
+    nvim \
+        +"Man $*" \
+        +'wincmd o' \
+        +'norm gO' \
+        +'wincmd L' \
+        +'wincmd w'
 }
