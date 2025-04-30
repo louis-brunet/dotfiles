bd870a00780d2bc1c28abcea3fb2bb5503f833b1
Louis Brunet <lbrunet@deloitte.fr>
2025-04-30 09:44:51 +0200

    fix(nvim): replace vim.hl with newer API vim.highlight

--- nvim/config/lua/user/autocommands.lua
+++ nvim/config/lua/user/autocommands.lua
@@ -2,7 +2,7 @@
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-        vim.hl.on_yank({})
+        vim.highlight.on_yank({})
     end,
     group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
     pattern = "*",
