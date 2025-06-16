dec1a87f1e34a42b66775187c7e5d5af8fb13c00
Louis Brunet <louis.jc.brunet@gmail.com>
2025-06-16 20:40:59 +0200

    chore(opencode): add logs to install script

--- opencode/install.sh
+++ opencode/install.sh
@@ -5,4 +5,9 @@ set -e
 # WARN: This makes the opencode installation specific to the current node version.
 # This will fail in projects that automatically load a different node version, or
 # when updating to a newer global node version, or when node has not been installed yet.
-npm install -g opencode-ai@latest
+if npm install -g opencode-ai@latest && opencode_version=$(opencode --version); then
+    echo "✅ installed opencode $opencode_version"
+else
+    echo "❌ could not install opencode"
+    exit 1
+fi
