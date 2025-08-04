70da4397bf7d234a003e0744cd5bdb124d117296
Louis Brunet <lbrunet@deloitte.fr>
2025-08-04 18:00:24 +0200

    chore(nvim): update plugins

--- nvim/config/lua/user/lazy-spec/ai/llama_vim.lua
+++ nvim/config/lua/user/lazy-spec/ai/llama_vim.lua
@@ -43,46 +43,46 @@ end
 
 ---@type LazySpec
 local M = {
-    {
-        "louis-brunet/llama.vim",
-
-        init = function()
-            -- NOTE: see :help llama_config
-            vim.g.llama_config = {
-                endpoint = llama_endpoints.enabled,
-                -- api_key = "",
-                -- n_prefix = 256,
-                -- n_suffix = 64,
-                -- n_predict = 128,
-                -- t_max_prompt_ms = 500,
-                -- t_max_predict_ms = 500,
-                -- show_info = 2,
-                -- auto_fim = true,
-                -- max_line_suffix = 8,
-                -- max_cache_keys = 250,
-                -- ring_n_chunks = 16,
-                -- ring_chunk_size = 64,
-                -- ring_scope = 1024,
-                -- ring_update_ms = 1000,
-            }
-            -- vim.fn.call("llama#init", {})
-
-            -- NOTE: customized llama.vim highlight llama_hf_info and
-            -- llama_hg_hint; see nvim/config/lua/user/lazy-spec/theme.lua
-
-            vim.api.nvim_create_user_command(
-                "LlmCompletionsToggle",
-                toggle_llm_completions,
-                { desc = "llama.vim: toggle completions" }
-            )
-
-            vim.api.nvim_create_user_command(
-                "LlmCompletionsStatus",
-                notify_llm_completions_status,
-                { desc = "llama.vim: show completions status" }
-            )
-        end,
-    },
+    -- {
+    --     "louis-brunet/llama.vim",
+    --
+    --     init = function()
+    --         -- NOTE: see :help llama_config
+    --         vim.g.llama_config = {
+    --             endpoint = llama_endpoints.enabled,
+    --             -- api_key = "",
+    --             -- n_prefix = 256,
+    --             -- n_suffix = 64,
+    --             -- n_predict = 128,
+    --             -- t_max_prompt_ms = 500,
+    --             -- t_max_predict_ms = 500,
+    --             -- show_info = 2,
+    --             -- auto_fim = true,
+    --             -- max_line_suffix = 8,
+    --             -- max_cache_keys = 250,
+    --             -- ring_n_chunks = 16,
+    --             -- ring_chunk_size = 64,
+    --             -- ring_scope = 1024,
+    --             -- ring_update_ms = 1000,
+    --         }
+    --         -- vim.fn.call("llama#init", {})
+    --
+    --         -- NOTE: customized llama.vim highlight llama_hf_info and
+    --         -- llama_hg_hint; see nvim/config/lua/user/lazy-spec/theme.lua
+    --
+    --         vim.api.nvim_create_user_command(
+    --             "LlmCompletionsToggle",
+    --             toggle_llm_completions,
+    --             { desc = "llama.vim: toggle completions" }
+    --         )
+    --
+    --         vim.api.nvim_create_user_command(
+    --             "LlmCompletionsStatus",
+    --             notify_llm_completions_status,
+    --             { desc = "llama.vim: show completions status" }
+    --         )
+    --     end,
+    -- },
 }
 
 return M
