83a448d52c70ce191b65c031c0e7c172baa3c9c2
Louis Brunet <lbrunet@deloitte.fr>
2026-03-30 10:09:53 +0200

    fix(tmux-sessionizer): restore previous ZLE input if line was not empty

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -2,6 +2,20 @@
 # alias tmux="TERM=xterm-256color tmux -u"
 alias tmux="TERM=tmux-256color tmux -u"
 
-# open tmux-sessionizer with Ctrl-F from outside tmux
-bindkey -s "^F" '^Utmux-sessionizer^M'
+
+### tmux-sessionizer
+
+function _run_tmux_sessionizer() {
+  if [[ $KEYMAP == "vicmd" ]]; then
+    zle -K viins          # force switch to insert mode
+  fi
+  zle push-input
+  BUFFER="tmux-sessionizer"
+  zle accept-line
+  zle pop-input
+}
+
+zle -N _run_tmux_sessionizer
+bindkey -M vicmd "^F" _run_tmux_sessionizer   # bind in normal mode
+bindkey -M viins "^F" _run_tmux_sessionizer   # bind in insert mode
 
